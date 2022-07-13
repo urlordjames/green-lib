31d0a23873cef8518dde04b8c1e3d9f3dacd3991
urlordjames <32751441+urlordjames@users.noreply.github.com>
2022-07-13 19:40:42 -0400

    add macos minecraft_path() implementation

--- src/util.rs
+++ src/util.rs
@@ -7,8 +7,7 @@ pub fn minecraft_path() -> std::path::PathBuf {
 		let appdata_path = std::path::Path::new(&appdata);
 		appdata_path.join(".minecraft")
 	} else if cfg!(target_os = "macos") {
-		//std::path::Path::new("~/Library/Application Support/minecraft").to_path_buf()
-		todo!("macos implementation");
+		std::path::Path::new("~/Library/Application Support/minecraft").to_path_buf()
 	} else {
 		let home = std::env::var("HOME").unwrap();
 		let home_path = std::path::Path::new(&home);
