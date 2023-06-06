d9b9c058c5ae0772629428003da570b41719ba61
urlordjames <32751441+urlordjames@users.noreply.github.com>
2023-06-06 13:07:31 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils }:
+		flake-utils.lib.eachDefaultSystem (system:
+			let pkgs = import nixpkgs {
+				inherit system;
+			}; in {
+				devShell = pkgs.mkShell {
+					buildInputs = with pkgs; [
+						cargo
+					];
+				};
+			}
+		);
+}
