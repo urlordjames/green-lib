e55164f655639d434aa6ceda10b3381e88086342
urlordjames <32751441+urlordjames@users.noreply.github.com>
2023-06-17 18:06:11 -0400

    use HOME environment variable for minecraft_path on macos

--- src/util.rs
+++ src/util.rs
@@ -1,13 +1,17 @@
 /// Gets the default minecraft path.
 /// # Warning
-/// MacOS implementation is currently broken.
+/// MacOS implementation is currently not tested.
 pub fn minecraft_path() -> std::path::PathBuf {
 	if cfg!(target_os = "windows") {
 		let appdata = std::env::var("APPDATA").unwrap();
 		let appdata_path = std::path::Path::new(&appdata);
 		appdata_path.join(".minecraft")
 	} else if cfg!(target_os = "macos") {
-		std::path::Path::new("~/Library/Application\\ Support/minecraft").to_path_buf()
+		let home = std::env::var("HOME").unwrap();
+		let home_path = std::path::Path::new(&home);
+		home_path.join("Library")
+			.join("Application Support")
+			.join("minecraft")
 	} else {
 		let home = std::env::var("HOME").unwrap();
 		let home_path = std::path::Path::new(&home);
